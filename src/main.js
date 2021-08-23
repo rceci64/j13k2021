d23527f7f133c57ab13dd12b8ec0aaa42961adb5
Roger Cecilia Costa <rogerceciliacosta@gmail.com>
2021-08-23 22:28:33 +0200

    NEW: imatge carregada. Prova de textures

--- src/main.js
+++ src/main.js
@@ -20,39 +20,47 @@ const mob = {
   color: 'rgba(236, 94, 103, 1)'
 }
 
+var img = new Image();   // Create new img element
+//img.addEventListener('load', function() {
+  
+//}, false);
+img.src = '../img/avatar.jpg'; // Set source path
+
 // game loop
 loop.start(function (dt) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   // update mob
   if (key.isDown(key.A)) {
-    mob.x = mob.x - (mob.speed * dt)
+    mob.x = mob.x - (mob.speed * dt);
   }
   if (key.isDown(key.D)) {
-    mob.x = mob.x + (mob.speed * dt)
+    mob.x = mob.x + (mob.speed * dt);
   }
   if (key.isDown(key.W)) {
-    mob.y = mob.y - (mob.speed * dt)
+    mob.y = mob.y - (mob.speed * dt);
   }
   if (key.isDown(key.S)) {
-    mob.y = mob.y + (mob.speed * dt)
+    mob.y = mob.y + (mob.speed * dt);
   }
 
   // check bounds collisions
   if (mob.x < 0) {
-    mob.x = canvas.width
+    mob.x = canvas.width;
   } else if (mob.x > canvas.width) {
-    mob.x = 0
+    mob.x = 0;
   }
   if (mob.y < 0) {
-    mob.y = canvas.height
+    mob.y = canvas.height;
   } else if (mob.y > canvas.height) {
-    mob.y = 0
+    mob.y = 0;
   }
 
   // draw mob
-  ctx.fillStyle = mob.color
-  ctx.fillRect(mob.x, mob.y, mob.width, mob.height)
+  ctx.fillStyle = mob.color;
+  ctx.fillRect(mob.x, mob.y, mob.width, mob.height);
+
+  ctx.drawImage(img, mob.x, mob.y);
 
   console.log('game update fn %s', dt)
 })
